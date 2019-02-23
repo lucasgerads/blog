23fbd8e7cce4df950c048bdc4135e9391646850e
Lucas Gerads <Lucas@Lucas-MacBook-Pro.local>
2019-02-23 09:34:41 +0100

    Initial Commit

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,35 @@
+---
+title: How I built and run this blog
+date: "2015-05-01T22:12:03.284Z"
+---
+
+This is my first blog post and mostly an experiment. It is mostly driven
+by my curiosity to learn a little bit about a "modern" web development stack and "severless""
+static websites. When you aren't a professional web developer it isn't easy to find your way around
+the myriad web frameworks, stacks, hosting options etc. So, for no particular good (or bad) reason my descion
+fell on [Gatsby](https://www.gatsbyjs.org/). So, here I am  documenting for you and for myself how to build this.
+
+
+## Gatsby 
+
+I am using [Kyle Mathews's](https://twitter.com/kylemathews) starter blog [template](https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-blog/). This 
+
+
+a href="https://www.gatsbyjs.org">Gatsby</a> and is based on the <a href="https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-blog/">Starter blog template</a> by <a href="https://twitter.com/kylemathews">Kyle Mathews
+
+This is my first post on my new fake blog! How exciting!
+
+I'm sure I'll write a lot more interesting things in the future.
+
+Oh, and here's a great quote from this Wikipedia on
+[salted duck eggs](http://en.wikipedia.org/wiki/Salted_duck_egg).
+
+> A salted duck egg is a Chinese preserved food product made by soaking duck
+> eggs in brine, or packing each egg in damp, salted charcoal. In Asian
+> supermarkets, these eggs are sometimes sold covered in a thick layer of salted
+> charcoal paste. The eggs may also be sold with the salted paste removed,
+> wrapped in plastic, and vacuum packed. From the salt curing process, the
+> salted duck eggs have a briny aroma, a gelatin-like egg white and a
+> firm-textured, round yolk that is bright orange-red in color.
+
+![Chinese Salty Egg](./salty_egg.jpg)
---
+++ src/pages/about.js
@@ -0,0 +1,57 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class NotFoundPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="About" />
+        <h1>About me</h1>
+        <p> 
+          I am interested in and write about, in descending authority: technology, business, science, entrepreneurship, politics, and life. All opinions expressed on this blog are my own.  </p>
+          I run the company <a href="http://aixcon.de">Aixcon PowerSystems GmbH</a> in Stolberg (Rhld.), Germany, spending
+          my days building power electronics and automation for various applications such as welding, coating and material treatment.
+           My lovely life partner Lydia helps me edit my posts. 
+        <h3>Feel free to contact me</h3>
+        <form name="contact" method="POST" netlify>
+            <p>
+                <label for="name">Your Name (not required): </label> <br />
+                <input type="text" name="name" />   
+            </p>
+            <p>
+                <label for="email">Your email address:</label>  <br />
+                <input type="email" name="email" />
+            </p>
+            <p>
+                <label for="message">Message: </label> <br />
+                <textarea name="message"></textarea>
+            </p>
+            <div data-netlify-recaptcha></div>
+            <p>
+                <button type="submit">Send</button>
+            </p>
+            </form>
+        <p>This site was built with <a href="https://www.gatsbyjs.org">Gatsby</a> and is based on the <a href="https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-blog/">Starter blog template</a> by <a href="https://twitter.com/kylemathews">Kyle Mathews</a>
+</p>
+      </Layout>
+    )
+  }
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
