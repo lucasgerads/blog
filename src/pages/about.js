130f31ceff0807e410812e9a5e29351fb311d8a0
Lucas Gerads <Lucas@Lucas-MacBook-Pro.local>
2019-02-23 14:03:55 +0100

    keep trying

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,19 +23,48 @@ class NotFoundPage extends React.Component {
           Germany, spending my days building power electronics and automation for
           various applications such as welding, coating and material treatment. My
           lovely life partner Lydia helps me edit my posts. </p>        
-      <h3>Feel free to contact me</h3>
-        <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
-          <p>
-            <label>Email: <input type="text" name="name" /></label>
+          <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
           </p>
           <p>
-            <label>Message: <textarea name="message"></textarea></label>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
           </p>
-          <div data-netlify-recaptcha="true"></div>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
+      </div>
+      
                 <p>This site was built with <a href="https://www.gatsbyjs.org">Gatsby</a> and is based on the <a href="https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-blog/">Starter blog template</a> by <a href="https://twitter.com/kylemathews">Kyle Mathews</a>
 </p>
       </Layout>
