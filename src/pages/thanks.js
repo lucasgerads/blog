86bcf084aa9c1657691c6c6e354feb889fe4f907
Lucas Gerads <Lucas@Lucas-MacBook-Pro.local>
2019-02-23 22:55:48 +0100

    adding thank you site

---
+++ src/pages/thanks.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class NotFoundPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="Thanks" />
+        <p> 
+            Thank you for reaching out to me!
+        </p>
+      </Layout>
+    )
+  }
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
