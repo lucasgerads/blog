1de4f2ecabd85243e58fd87fa79de143f4a29526
Lucas Gerads <Lucas@Lucas-MacBook-Pro.local>
2019-02-24 00:02:18 +0100

    small fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import { navigateTo } from "gatsby-link";
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
